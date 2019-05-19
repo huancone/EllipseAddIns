38692a7ec7038a38361824e03f3cc8ce14536d31
Héctor Hernández <hekutoru2kx@users.noreply.github.com>
2019-05-19 15:07:31 -0500

    Desarrollo de AddIn de Consolidaciones
    Se crea (fuente extraviada) el AddIn de Consolidaciones, antiguamente desarrollado por Hussein Villamizar.
    
    * Consolida StockCodes
    * Actualiza el campo de Service Product Id

---
+++ Materiales/EllipseConsolidacionesExcelAddIn/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EllipseConsolidacionesExcelAddIn")]
+[assembly: AssemblyDescription("Loader de Consolidaciones y Categoría de Servicios para StockCodes")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hambings SAS / Cerrejón Ltda")]
+[assembly: AssemblyProduct("EllipseConsolidacionesExcelAddIn")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7d6034f8-5785-4623-9b79-2d0fee270c83")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
+
