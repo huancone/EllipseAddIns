3ebf1676d720b36b978f3c6ee320816b6cf304e8
Héctor Hernández <hekutoru2kx@gmail.com>
2021-03-19 10:52:55 -0500

    Encryption Update
    PSI

--- SharedClassLibrary/SharedClassLibrary.Utilities.Encryption/Encryption.cs
+++ SharedClassLibrary/SharedClassLibrary.Utilities.Encryption/Encryption.cs
@@ -50,12 +50,13 @@ namespace SharedClassLibrary.Utilities.Encryption
                             using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
                             {
                                 cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
-                                cryptoStream.FlushFinalBlock();
+                                
                                 // Create the final bytes as a concatenation of the random salt bytes, the random iv bytes and the cipher bytes.
                                 var cipherTextBytes = saltStringBytes;
                                 cipherTextBytes = cipherTextBytes.Concat(ivStringBytes).ToArray();
                                 cipherTextBytes = cipherTextBytes.Concat(memoryStream.ToArray()).ToArray();
                                 memoryStream.Close();
+                                cryptoStream.FlushFinalBlock();
                                 cryptoStream.Close();
                                 return Convert.ToBase64String(cipherTextBytes);
                             }
@@ -101,6 +102,7 @@ namespace SharedClassLibrary.Utilities.Encryption
                                 var plainTextBytes = new byte[cipherTextBytes.Length];
                                 var decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
                                 memoryStream.Close();
+                                cryptoStream.FlushFinalBlock();
                                 cryptoStream.Close();
                                 return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
                             }
